f870e7971eb07dc97d2e72bb67ec2f4cd59e520c
Jan Moritz Meyer <j.meyer@makeitfix.com>
2022-08-13 11:22:18 +0200

    JM | add websockets

--- hello.py
+++ hello.py
@@ -1,7 +1,17 @@
 from flask import Flask, render_template
+from flask_sock import Sock
 
 app = Flask(__name__)
+sock = Sock(app)
 
 @app.route("/")
 def hello_world():
     return render_template("index.html")
+
+@sock.route('/room/<string:roomname>')
+def echo(ws, roomname):
+    while True:
+        data = ws.receive()
+        ws.send(roomname)
+
+app.run(host='0.0.0.0', port=2022)
