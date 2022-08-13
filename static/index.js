364cc551770c64f2aa345e46f84fbc1ef19bdc8c
asqiir <>
2022-08-13 16:04:14 +0200

    menu open/clos on /rainbow/*

---
+++ static/index.js
@@ -0,0 +1,14 @@
+window.onload = function() {
+    document.getElementById("otherbutton").style.visibility = "hidden";
+}
+function toggleMenu() {
+    var menu = document.getElementById("menu");
+    var otherbutton = document.getElementById("otherbutton");
+    if (menu.style.visibility == "hidden") {
+        menu.style.visibility = "visible";
+        otherbutton.style.visibility = "hidden";
+    } else {
+        menu.style.visibility = "hidden";
+        otherbutton.style.visibility = "visible";
+    }
+}
