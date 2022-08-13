3404f4348284dadec3b2da49035aea1d2b957117
Jan Moritz Meyer <j.meyer@makeitfix.com>
2022-08-13 14:17:19 +0200

    JM | changees

--- hello.py
+++ hello.py
@@ -1,17 +1,54 @@
 from flask import Flask, render_template
-from flask_sock import Sock
+from flask_socketio import SocketIO, emit, send
 
 app = Flask(__name__)
-sock = Sock(app)
+socketio = SocketIO(app)
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html")
+@app.route('/')
+def index():
+    return render_template('index.html')\
 
-@sock.route('/room/<string:roomname>')
-def echo(ws, roomname):
-    while True:
-        data = ws.receive()
-        ws.send(roomname)
+@app.route('/rainbow/<string:roomName>')
+def rainbow(roomName):
+    return render_template('rainbow.html')
 
-app.run(host='0.0.0.0', port=2022)
+@app.route('/room/<string:roomName>')
+def room(roomName):
+    print(roomName)
+    return render_template('index.html')
+
+from flask_socketio import join_room, leave_room
+
+@socketio.on('join')
+def on_join(data):
+    username = data['username']
+    room = data['room']
+    join_room(room)
+    send(username + ' has entered the room.', to=room)
+    emit("change", to=room)
+
+@socketio.on('leave')
+def on_leave(data):
+    username = data['username']
+    room = data['room']
+    leave_room(room)
+    send(username + ' has left the room.', to=room)
+
+@socketio.on('connect')
+def test_connect():
+    emit('my response', {'data': 'Connected'})
+
+@socketio.on('disconnect')
+def test_disconnect():
+    print('Client disconnected')
+
+@socketio.on('disconnect')
+def test_disconnect():
+    print('Client disconnected')
+
+@socketio.on('change')
+def lol():
+    emit('change', to="moin")
+
+if __name__ == '__main__':
+    socketio.run(app, port=8080, host="0.0.0.0")
